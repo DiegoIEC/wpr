29d6e1bae88e8303a398de82d69d305109ce2577
Bartvb0 <21057788@student.hhs.nl>
2023-12-14 23:33:54 +0100

    Navigatie balk alignment

--- web/src/Navbar.js
+++ web/src/Navbar.js
@@ -1,12 +1,13 @@
+import './Navbar.css';
 const NavBar = () => {
     return (
           <nav className="navbar">
             <div className="navbar-content">
-              <h1>Accessibility</h1>
-              <div className="links">
+              <h1 className="title">Accessibility</h1>
+              <div className="navbar-right">
                   <a href="/">Home</a>
                   <a href="/account">Mijn account</a>
-                  <a href="/research">Onderzoeken</a>
+                  <a href="./Onderzoeken.js">Onderzoeken</a>
                   <a href="/settings">Instellingen</a>
                   <a href="/logout">Uitloggen</a>
               </div>
