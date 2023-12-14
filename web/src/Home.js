f4b5445b2b879c265ad26d71a494a2326502fc85
Bartvb0 <21057788@student.hhs.nl>
2023-12-14 22:34:20 +0100

    Interim update Homepage

--- web/src/Home.js
+++ web/src/Home.js
@@ -1,3 +1,4 @@
+import './Home.css';
 const userName = "gebruiker"
 const ongoingResearch = ['Onderzoek 1', 'Onderzoek 2']
 const completedResearchCount = 100
@@ -6,31 +7,31 @@ const Home = () => {
     
     return (
         <div className="home">
-            <section className="welcome">
-            <h2>Welkom {userName}</h2>
-            <p>Hieronder een overzicht met onderzoeken waarvoor u in aanmerking komt</p>
-            <button> Onderzoeken </button>
+            <section className="welcome section-background-blue text-color-green">
+            <h2 className="center-text">Welkom {userName}</h2>
+            <p className="center-text">Hieronder een overzicht met onderzoeken waarvoor u in aanmerking komt</p>
+            <button className="center-button button-green"> Onderzoeken </button>
           </section>
-          <section className="latest-news">
-            <h2>Laatste nieuws</h2>
+          <section className="latest-news section-background-green text-color-blue">
+            <h2 className="center-text">Laatste nieuws</h2>
             <div className="news-content">
               {/* Content for latest news */}
             </div>
           </section>
-          <section className="ongoing-research">
-            <h2>Lopende onderzoeken</h2>
-            <ul>
+          <section className="ongoing-research text-color-green">
+            <h2 className="center-text">Lopende onderzoeken</h2>
+            <ul className="center-text">
               {ongoingResearch.map((research, index) => (
                 <li key={index}>{research}</li>
               ))}
             </ul>
           </section>
-          <section className="contribution">
-            <h2>Uw bijdrage</h2>
-            <p>Bekijk de verschillende onderzoeken die u zijn uitgevoerd</p>
-            <button>Bekijk details</button>
-            <div className="contribution-stats">
-              <div>U heeft meegedaan aan zoveel onderzoeken {completedResearchCount}</div>
+          <section className="contribution section-background-green text-color-blue">
+            <h2 className="center-text">Uw bijdrage</h2>
+            <p className="center-text">Bekijk de verschillende onderzoeken die u zijn uitgevoerd</p>
+            <button className="center-button button-blue">Bekijk details</button>
+            <div className="contribution-stats-blue">
+              <div>U heeft meegedaan aan {completedResearchCount} onderzoeken</div>
               <div>Ontvangen compensatie.. €{compensation}</div>
             </div>
           </section>
--- web/src/Navbar.js
+++ web/src/Navbar.js
@@ -1,13 +1,15 @@
 const NavBar = () => {
     return (
           <nav className="navbar">
-            <h1>Accessibility</h1>
-            <div className="links">
-                <a href="/">Home</a>
-                <a href="/account">Mijn account</a>
-                <a href="/research">Onderzoeken</a>
-                <a href="/settings">Instellingen</a>
-                <a href="/logout">Uitloggen</a>
+            <div className="navbar-content">
+              <h1>Accessibility</h1>
+              <div className="links">
+                  <a href="/">Home</a>
+                  <a href="/account">Mijn account</a>
+                  <a href="/research">Onderzoeken</a>
+                  <a href="/settings">Instellingen</a>
+                  <a href="/logout">Uitloggen</a>
+              </div>
             </div>
           </nav>
     );
