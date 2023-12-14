146d6ac9639e201c9c144b371239f3704f13ba14
Diego <53939730+DiegoIEC@users.noreply.github.com>
2023-12-14 21:52:22 +0100

    router en onderzoek pagina toegevoegd

--- web/src/App.js
+++ web/src/App.js
@@ -1,18 +1,28 @@
 import NavBar from './Navbar';
 import Home from './Home';
 import Footer from './Footer';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Onderzoeken from './Onderzoeken';
 
 function App() {
   return (
+    <Router>
     <div className="App">
         <header className="App-header">
           <NavBar />
         </header>
         <main>
-          <Home/>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+        <Routes>
+          <Route path="/onderzoeken" element={<Onderzoeken />}></Route>
+        </Routes>
+          
         </main>
         <Footer />
       </div>
+      </Router>
   );
 }
 
