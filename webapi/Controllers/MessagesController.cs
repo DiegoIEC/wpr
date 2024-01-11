b3aafd021f2f8a66189edaae10eb5505864bec41
Diego <53939730+DiegoIEC@users.noreply.github.com>
2024-01-11 19:26:46 +0100

    test messages

--- webapi/Controllers/MessagesController.cs
+++ webapi/Controllers/MessagesController.cs
@@ -19,13 +19,49 @@ namespace webapi.Controllers
             _context = context;
         }
 
-        // Other methods...
+        // GET: api/Messages
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MessageDTO>>> GetMessages()
+        {
+            return await _context.Messages
+                .Select(m => new MessageDTO
+                {
+                    MessageId = m.MessageId,
+                    Content = m.Content,
+                    Timestamp = m.Timestamp,
+                    SenderId = m.SenderId,
+                    ReceiverId = m.ReceiverId
+                })
+                .ToListAsync();
+        }
 
-        // POST: api/Onderzoek/Messages
-        [HttpPost("Messages")]
+        // GET: api/Messages/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<MessageDTO>> GetMessage(int id)
+        {
+            var message = await _context.Messages.FindAsync(id);
+
+            if (message == null)
+            {
+                return NotFound();
+            }
+
+            var messageDTO = new MessageDTO
+            {
+                MessageId = message.MessageId,
+                Content = message.Content,
+                Timestamp = message.Timestamp,
+                SenderId = message.SenderId,
+                ReceiverId = message.ReceiverId
+            };
+
+            return messageDTO;
+        }
+
+        // POST: api/Messages
+        [HttpPost]
         public async Task<ActionResult<MessageDTO>> PostMessage(MessageDTO messageDTO)
         {
-            // Create a new Message entity using the DTO
             var message = new Message
             {
                 Content = messageDTO.Content,
@@ -37,33 +73,69 @@ namespace webapi.Controllers
             _context.Messages.Add(message);
             await _context.SaveChangesAsync();
 
-            // Return the created MessageDTO
             return CreatedAtAction(nameof(GetMessage), new { id = message.MessageId }, messageDTO);
         }
 
-        // Other methods...
-
-        // Helper method to get a Message by ID
-        private async Task<MessageDTO> GetMessage(int id)
+        // PUT: api/Messages/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMessage(int id, MessageDTO messageDTO)
         {
-            var message = await _context.Messages.FindAsync(id);
-
-            if (message == null)
+            if (id != messageDTO.MessageId)
             {
-                return null;
+                return BadRequest();
             }
 
-            // Convert the Message entity to MessageDTO
-            var messageDTO = new MessageDTO
+            var message = await _context.Messages.FindAsync(id);
+            if (message == null)
             {
-                MessageId = message.MessageId,
-                Content = message.Content,
-                Timestamp = message.Timestamp,
-                SenderId = message.SenderId,
-                ReceiverId = message.ReceiverId
-            };
+                return NotFound();
+            }
 
-            return messageDTO;
+            message.Content = messageDTO.Content;
+            message.Timestamp = messageDTO.Timestamp;
+            message.SenderId = messageDTO.SenderId;
+            message.ReceiverId = messageDTO.ReceiverId;
+
+            _context.Entry(message).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MessageExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/Messages/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMessage(int id)
+        {
+            var message = await _context.Messages.FindAsync(id);
+            if (message == null)
+            {
+                return NotFound();
+            }
+
+            _context.Messages.Remove(message);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool MessageExists(int id)
+        {
+            return _context.Messages.Any(m => m.MessageId == id);
         }
     }
 }
