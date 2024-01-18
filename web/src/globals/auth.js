7c22e91af07606cff9998448795b9170f8f62b1c
Bartvb0 <21057788@student.hhs.nl>
2024-01-18 16:39:22 +0100

    Update auth.js

--- web/src/globals/auth.js
+++ web/src/globals/auth.js
@@ -5,15 +5,6 @@ const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
 
-  useEffect(() => {
-    const storedUser = localStorage.getItem('user');
-    if (storedUser) {
-      console.log(JSON.parse(storedUser));
-      setUser(storedUser);
-    }
-    
-  }, []);
-
   const login_user = async (userData) => {
     setUser(userData);
   };
