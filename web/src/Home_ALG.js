b79cc53cdf07e0218f2f3ec81e1ac5db3451e4b1
Bartvb0 <21057788@student.hhs.nl>
2024-01-23 11:28:13 +0100

    Update Home_ALG.js

--- web/src/Home_ALG.js
+++ web/src/Home_ALG.js
@@ -11,7 +11,7 @@ const Home_ALG = () => {
     return (
         <div className="home">
           <SiteModeButton/>
-            <section>
+            <section className="welcome section-background text-color-black block-content">
             <div className="block-left format-div">
             <h2 className="center-text format-title">Welkom bij Stichting Accessibility</h2>
             <p className="center-text">Het verbeteren van digitale accessibility voor iedereen</p>
