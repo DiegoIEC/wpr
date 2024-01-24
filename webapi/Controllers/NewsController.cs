039e5d4586fb734e2825e63c100421d6c5cbccb1
Bartvb0 <21057788@student.hhs.nl>
2024-01-24 13:39:17 +0100

    Update NewsController.cs

--- webapi/Controllers/NewsController.cs
+++ webapi/Controllers/NewsController.cs
@@ -10,28 +10,35 @@ namespace webapi.Controllers
     [Route("api/[controller]")]
     [ApiController]
     public class NewsController : ControllerBase{
+        private readonly HttpClient _httpClient;
 
+        public NewsController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
         
         // GET: api/News
         [HttpGet]
         public async Task<object> GetNews()
         {
             try{
-                string keywords = HttpUtility.UrlEncode("online accessibility");
-                string apiKey = "089c8f2e2dfe4d818e24dcf00a2bb122";
-                var url = $"https://newsapi.org/v2/everything?q={keywords}&sortBy=publishedAt&apiKey={apiKey}";
-                //https://newsapi.org/v2/everything?q=online-accessibility&apiKey=089c8f2e2dfe4d818e24dcf00a2bb122
-                using HttpClient client = new();
+                //https://newsapi.org/v2/everything?q=+online accessibility&apiKey=089c8f2e2dfe4d818e24dcf00a2bb122
+                //string keywords = "online accessibility";
+                //string encodedKeywords = HttpUtility.UrlEncode(keywords);
+                //string apiKey = "089c8f2e2dfe4d818e24dcf00a2bb122";
+
+                var url = "https://newsapi.org/v2/everything?q=online accessibility&sortBy=publishedAt&apiKey=089c8f2e2dfe4d818e24dcf00a2bb122";
+            
                 try{
-                    string response = await client.GetStringAsync(url);
-                    return response;
+                    string response = await _httpClient.GetStringAsync(url);
+                    return Ok(response);
                 }
                 catch (HttpRequestException e){
-                    return $"Error: {e.Message}";
+                    return BadRequest(e.Message);
                 }
             }
             catch (Exception e){
-                return e.ToString();
+                return StatusCode(500, e.ToString());
             }
         }
     }
