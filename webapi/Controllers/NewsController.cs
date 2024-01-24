a69893de30f306baaf4e1f801d573a9bc8a11b71
Bartvb0 <21057788@student.hhs.nl>
2024-01-24 13:27:22 +0100

    Update NewsController.cs

--- webapi/Controllers/NewsController.cs
+++ webapi/Controllers/NewsController.cs
@@ -3,6 +3,7 @@ using System.Net;
 using DemoApp.Data;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using System.Web;
 
 namespace webapi.Controllers
 {
@@ -13,23 +14,25 @@ namespace webapi.Controllers
         
         // GET: api/News
         [HttpGet]
-        public async Task<string> GetNews()
+        public async Task<object> GetNews()
         {
             try{
-                var url = "https://newsapi.org/v2/top-headlines?" +
-                            "q=Accessibility&" +
-                            "apiKey=089c8f2e2dfe4d818e24dcf00a2bb122";
-
-                var json = await new HttpClient().GetStringAsync(url);
-                if (json != null){
-                    return json;
+                string keywords = HttpUtility.UrlEncode("online accessibility");
+                string apiKey = "089c8f2e2dfe4d818e24dcf00a2bb122";
+                var url = $"https://newsapi.org/v2/everything?q={keywords}&sortBy=publishedAt&apiKey={apiKey}";
+                //https://newsapi.org/v2/everything?q=online-accessibility&apiKey=089c8f2e2dfe4d818e24dcf00a2bb122
+                using HttpClient client = new();
+                try{
+                    string response = await client.GetStringAsync(url);
+                    return response;
                 }
-                return "Helaas";
+                catch (HttpRequestException e){
+                    return $"Error: {e.Message}";
+                }
             }
             catch (Exception e){
                 return e.ToString();
             }
         }
-
     }
 }
